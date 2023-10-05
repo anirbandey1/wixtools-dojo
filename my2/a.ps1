9f269ba4c7bc806298976720686c78ce0a7e6fbb
Anirban Dey <116617392+anirbandey1@users.noreply.github.com>
2023-10-05 16:32:39 +0530

    using fragments

--- my2/a.ps1
+++ my2/a.ps1
@@ -1,3 +1,4 @@
 Write-Host "This is powershell script installed using WiX"
+Write-Host "Well, started using fragments"
 Write-Host -NoNewLine "Please any key to continue ..."
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
