ea42d5f3812d931605cec1a7cd5227f183faf788
Anirban Dey <116617392+anirbandey1@users.noreply.github.com>
2023-10-05 16:14:34 +0530

    component group

---
+++ my2/a.ps1
@@ -0,0 +1,3 @@
+Write-Host "This is powershell script installed using WiX"
+Write-Host -NoNewLine "Please any key to continue ..."
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
---
+++ my2/dojostart.cmd
@@ -0,0 +1,5 @@
+
+start C:\dojo
+start appwiz.cpl
+
+msiexec /i C:\dojo\my2\op.msi /l*v C:\dojo\logs/install.txt
